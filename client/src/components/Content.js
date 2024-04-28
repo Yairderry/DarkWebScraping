165faeab7df0e1ad153952123a36d44d28178fc5
Yairderry <77237113+Yairderry@users.noreply.github.com>
2024-04-28 14:31:55 +0300

    Added read more functionality to pastes

--- client/src/components/Content.js
+++ client/src/components/Content.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Content({ title, content }) {
+  const style = {
+    WebkitLineClamp: 4,
+    WebkitBoxOrient: "vertical",
+    overflow: "hidden",
+    display: "-webkit-box",
+  };
+  const [isReadLess, setIsReadLess] = useState(true);
   return (
     <div className="paste-content">
-      <h3>{title}</h3>
-      <p>{content}</p>
+      <h3 className="paste-title">{title}</h3>
+      <p style={isReadLess ? style : {}}>{content}</p>
+      <p
+        className="paste-button"
+        onClick={() => setIsReadLess(!isReadLess)}
+      >{`Read ${isReadLess ? "more" : "less"}`}</p>
     </div>
   );
 }
